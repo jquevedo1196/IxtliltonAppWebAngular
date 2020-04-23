5654673353f824b6cee6ef5da1b91ba64a185be3
Jesus Quevedo <jenrique_qt@hotmail.com>
2020-04-23 12:24:32 -0500

    Listo para deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IxtliltonV2
+# IxtliltonAppAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'IxtliltonV2'`, () => {
+  it(`should have as title 'IxtliltonAppAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IxtliltonV2');
+    expect(app.title).toEqual('IxtliltonAppAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('IxtliltonV2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('IxtliltonAppAngular app is running!');
   });
 });
